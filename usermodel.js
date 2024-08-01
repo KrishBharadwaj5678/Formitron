5941eb8f26c127ddf435bb0b9dec023c68b9bfc8
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-08-01 13:47:50 +0530

    Update usermodel.js

--- usermodel.js
+++ usermodel.js
@@ -2,6 +2,18 @@ let mongoose=require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/StudentRegistration")
 
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://test:test1234@cluster0.h6ocrk9.mongodb.net/Student?retryWrites=true&w=majority&ssl=true', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
 let Schema=mongoose.Schema({
     FirstName:'String',
     LastName:'String',
